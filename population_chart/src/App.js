6f0f9a7f9ef10c8c1069f4d98b11ec717a1f1bb1
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-09-11 18:03:21 +0530

    Implemented with React

--- population_chart/src/App.js
+++ population_chart/src/App.js
@@ -4,11 +4,62 @@ import './App.css';
 import Chart from './components/Chart'
 
 class App extends Component {
+
+    constructor(props){
+      super(props)
+      this.state = {
+        chartData : {}
+      }
+    }
+
+  componentWillMount(){
+    this.getData()
+  }
+
+  getData(){
+
+    console.log("getDataCalled")
+    // Ajax Call Happens here from External API's
+    this.setState({
+      chartData: {
+        labels: ["Toronto", "Montreal", "Vancouver" , "Calgary"],
+                
+        datasets: [{
+            label: "Population",
+            data : [
+                2800000,
+                1700000,
+                648000,
+                1270000
+            ],
+            backgroundColor: [
+                'green',
+                'red',
+                'blue',
+                'yellow'
+            ],
+            borderWidth : 4,
+            borderColor : 'white',
+            hoverBorderWidth : 3,
+            hoverBorderColor : "black"
+        }]
+      }
+    })
+  }
+
   render() {
+
+
+
+
     return (
       <div className="App">
-          <img src={logo} className="App-logo" alt="logo" />
-     <Chart/>
+     <img src={logo} className="App-logo" alt="logo" />
+     <Chart chartData={this.state.chartData}/>
+     <img src={logo} className="App-logo" alt="logo" />
+
+     <Chart chartData={this.state.chartData}/>
+
       </div>
     );
   }
--- population_chart/src/components/Chart.js
+++ population_chart/src/components/Chart.js
@@ -6,65 +6,11 @@ class Chart extends Component {
     constructor(props){
         super(props)
         this.state = {
-            chartData :{
-                
-                    labels: ["Toronto", "Montreal", "Vancouver" , "Calgary"],
-                
-                    datasets: [{
-                        label: "Population",
-                        data : [
-                            2800000,
-                            1700000,
-                            648000,
-                            1270000
-                        ],
-                        backgroundColor: [
-                            'green',
-                            'red',
-                            'blue',
-                            'yellow'
-                        ],
-                        borderWidth : 4,
-                        borderColor : 'white',
-                        hoverBorderWidth : 3,
-                        hoverBorderColor : "black"
-                    }]
-                },
-                options:{
-                    title: {
-                        display:true,
-                        text: "Largest Cities In Canada",
-                        fontSize : 30
-                    },
-                    legend : {
-                        labels:{
-                            fontColor: "green",
-                        }
-                        
-                    },
-                    layout  : {
-                        padding:{
-                            left:50,
-                            top:10000,
-                            right: 0,
-                            bottom: 0
-
-                        }
-                        
-                    },
-                    tooltips:{
-                            enabled: true
-                        }
-                }
+            chartData : this.props.chartData
         }
     }
 
-    render(){
-
-
-
-                    
-             
+    render(){          
         return(<div>
             <Bar
                 data={this.state.chartData}
